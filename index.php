86288dac0ca7794e20a7ccb2afc5b64cb7b229e6
Alvin Lesmana <alvin.lesmana88@gmail.com>
2021-09-06 15:17:11 +0700

    header done

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Niagahoster</title>
+    <!-- Bootstrap core CSS -->
+    <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/css/niagahoster.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="container-fluid nv-top-menu">
+        <div class="container">
+            <div class="navbar-brand small-ads">
+                <div class="pull-left">
+                    <div class="pull-left glyphicon glyphicon-bookmark nv-bookmark nv-space-icon">
+                    </div>
+                    <div class="pull-left" style="margin-top:2px"> Gratis Ebook 9 Cara Cerdas Menggunakan Domain [x]
+                    </div>
+                </div>
+                <div class="pull-right" style="margin-top:2px">
+                    <span class="glyphicon glyphicon-earphone nv-space-icon"></span> <span class="nv-top-info">0274-5305505</span>
+                    <span class="glyphicon glyphicon-headphones nv-space-icon"></span> <span class="nv-top-info">Live Chat</span>
+                    <span class="glyphicon glyphicon-user nv-space-icon"></span> <span class="nv-top-info">Member Area</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid nv-header">
+        <nav class="navbar container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img src="./assets/images/niagahoster-logo.png">
+                </a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul id="nv-menu" class="nav navbar-nav">
+                    <li><a href="#">Hosting</a></li>
+                    <li><a href="#">Domain</a></li>
+                    <li><a href="#">Server</a></li>
+                    <li><a href="#">Website</a></li>
+                    <li><a href="#">Afiliasi</a></li>
+                    <li><a href="#">Promo</a></li>
+                    <li><a href="#">Pembayaran</a></li>
+                    <li><a href="#">Review</a></li>
+                    <li><a href="#">Kontak</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </nav>
+        <!--/.container -->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
